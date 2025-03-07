8591429818eac0c1e2c23baeccb861e88a28484d
batman8056 <manimanish8056@gmail.com>
2025-03-07 13:07:09 +0530

    dsa update

--- leetCode-Practice/1-big-o-notation.py
+++ leetCode-Practice/1-big-o-notation.py
@@ -1,5 +1,5 @@
 # If we don't know what number in the list
-# the loop taken n-number of item in the list to find out linerar o(n) (time and space equal)
+# the loop taken n-number of item in the list to find out linera o(n) (time and space equal)
 
 def check_number(numbers):
     for num in numbers:
@@ -9,6 +9,6 @@ def check_number(numbers):
             continue
     return f"number {2} is not present"  # Return False if 2 is not found
 
-# Example usage
+# Example number of list
 number = [1, 3, 4, 2, 5]
 print(check_number(number))  # Output: True
